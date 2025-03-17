92aae77de54797214b44358325a00e921c0b851a
germa <germa@GERMANJR-10>
2025-03-17 00:15:19 -0400

    Add project files.

---
+++ Promedio de notas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace promedio_de_calificaciones;
+class Program
+{
+    static void Main()
+    {
+        string nombre, matricula;
+        double suma = 0, promedio, nota;
+
+        Console.WriteLine("Ingrese el nombre del estudiante:");
+        nombre = Console.ReadLine();
+
+        Console.WriteLine("Ingrese la matrícula del estudiante:");
+        matricula = Console.ReadLine();
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine("Ingrese la nota del estudiante " + i + ":");
+            nota = Convert.ToDouble(Console.ReadLine());
+            suma += nota;
+        }
+
+        promedio = suma / 5;
+
+        Console.WriteLine("------------ Boletín ------------");
+        Console.WriteLine("El promedio del estudiante " + nombre + " es de: " + promedio);
+    }
+}
+
+
